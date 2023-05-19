a11290bc41f6d464e90792e9df2a9517237d9835
Acerola-BP11 <felipegrego23@outlook.com>
2023-05-18 21:37:00 -0300

    First Commit

---
+++ src/main.js
@@ -0,0 +1,17 @@
+let magia = {
+    dano,
+    alcance,
+    alvosAfetados,
+    area,
+    ritual,
+    tipo,
+    tipoAtaque,
+    testeResistencia,
+    tempoConjuracao,
+    concentracao,
+    componentes,
+    descricao,
+    elemento
+}
+
+fire_bolt = new magia('1d8', '60', '1', 'individual')
